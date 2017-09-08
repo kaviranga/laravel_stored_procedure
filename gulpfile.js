a3deec47c9ea38f8c9b485f58c2103aa2f7ce502
Duminda Kaviranga Gunawardhana <kaviranga5955@gmail.com>
2017-09-08 17:28:11 +0530

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,12 @@ var elixir = require('laravel-elixir');
  |
  */
 
+require('laravel-elixir-vue-2')
+
+elixir(function(mix) {
+  mix.webpack('app.js'); // resources/assets/js/app.js
+})
+
 elixir(function(mix) {
     mix.sass('app.scss');
 });
